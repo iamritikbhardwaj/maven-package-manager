d7933d917d92b2bcac69909746182b0bcb3f1664
ritik <ritiklrt2@gmail.com>
2025-08-27 16:17:03 +0530

    made a basic maven package manager

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iamritikbhardwaj/maven_package_manager
+
+go 1.24.6
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Project struct {
+	XMLName      xml.Name      `xml:"project"`
+	Dependencies *Dependencies `xml:"dependencies"`
+}
+
+type Dependencies struct {
+	DependencyList []Dependency `xml:"dependency"`
+}
+
+type Dependency struct {
+	GroupID    string `xml:"groupId"`
+	ArtifactID string `xml:"artifactId"`
+	Version    string `xml:"version"`
+}
+
+func main() {
+	if len(os.Args) != 4 {
+		fmt.Println("Usage: go run main.go <groupId> <artifactId> <version>")
+		return
+	}
+
+	groupId := os.Args[1]
+	artifactId := os.Args[2]
+	version := os.Args[3]
+
+	file, err := os.Open("pom.xml")
+	if err != nil {
+		fmt.Println("Error opening pom.xml:", err)
+		return
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println("Error reading pom.xml:", err)
+		return
+	}
+
+	var project Project
+	if err := xml.Unmarshal(data, &project); err != nil {
+		fmt.Println("Error parsing XML:", err)
+		return
+	}
+
+	if project.Dependencies == nil {
+		project.Dependencies = &Dependencies{}
+	}
+
+	found := false
+	for i, dep := range project.Dependencies.DependencyList {
+		if dep.GroupID == groupId && dep.ArtifactID == artifactId {
+			project.Dependencies.DependencyList[i].Version = version
+			found = true
+			fmt.Println("Dependency exists. Updating version.")
+			break
+		}
+	}
+
+	if !found {
+		newDep := Dependency{
+			GroupID:    groupId,
+			ArtifactID: artifactId,
+			Version:    version,
+		}
+		project.Dependencies.DependencyList = append(project.Dependencies.DependencyList, newDep)
+		fmt.Println("Dependency added.")
+	}
+
+	output, err := xml.MarshalIndent(project, "", "  ")
+	if err != nil {
+		fmt.Println("Error marshalling XML:", err)
+		return
+	}
+
+	// Add XML header
+	finalOutput := []byte(xml.Header + string(output))
+
+	if err := ioutil.WriteFile("pom.xml", finalOutput, 0644); err != nil {
+		fmt.Println("Error writing pom.xml:", err)
+		return
+	}
+
+	fmt.Println("pom.xml updated.")
+}
